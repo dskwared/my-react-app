64b73d16185745e2da17ec7d5131aecfbf5a20d8
Doug DeFrank <{ID}+{username}@users.noreply.github.com>
2024-06-07 11:35:49 -0400

    Updates as of 2024-JUN-07

---
+++ src/books.js
@@ -0,0 +1,27 @@
+// Module 47 - Import local images from /src
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+// Module 33 - Creating a book array
+// Module 46 - Move the book array from index.js to books.js
+export const books = [
+  {
+    title: 'Dad, I Want to Hear Your Story',
+    author: 'Jeffrey Mason',
+    img: img1,
+    id: 1,
+  },
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    img: img2,
+    id: 2,
+  },
+  {
+    title: 'Ready Player One',
+    author: 'Earnest Cline',
+    img: img3,
+    id: 3,
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// My First Component from Udemy Course
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Because these are JavaScript files, we don't need to add the .js extension on the file names.
+// These are ES6 modules (not specific to Vanilla JS or React)
+import { books } from './books'
+import Book from './book'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <>
+      <h1>best sellers: best books</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          // const { img, title, author, id } = book
+          // return <Book img={img} title={title} author={author} key={id} />
+          return <Book {...book} key={book.id} number={index} />
+        })}
+      </section>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList />)
